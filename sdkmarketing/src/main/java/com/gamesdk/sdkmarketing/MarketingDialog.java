0306fbb804cb47f1afa162561f9bd60320b223e4
amurocrash <amurocrash@126.com>
2016-05-03 14:47:27 +0800

    test resource

--- sdkmarketing/src/main/java/com/gamesdk/sdkmarketing/MarketingDialog.java
+++ sdkmarketing/src/main/java/com/gamesdk/sdkmarketing/MarketingDialog.java
@@ -1,8 +1,41 @@
 package com.gamesdk.sdkmarketing;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.cngame.gamesdklib.mvp.view.animator.AnimatorBottomExit;
+import com.cngame.gamesdklib.mvp.view.animator.AnimatorTopEnter;
+import com.cngame.gamesdklib.mvp.view.animator.BaseAnimatorSet;
+import com.cngame.gamesdklib.mvp.view.dialog.SDKBaseAnimationDialog;
+
 /**
  * Created by Amuro on 2016/5/3.
  */
-public class MarketingDialog
+public class MarketingDialog extends SDKBaseAnimationDialog
 {
+    public MarketingDialog(Context context)
+    {
+        super(context);
+    }
+
+    @Override
+    protected BaseAnimatorSet getShowAnim()
+    {
+        return new AnimatorTopEnter();
+    }
+
+    @Override
+    protected BaseAnimatorSet getDismissAnim()
+    {
+        return new AnimatorBottomExit();
+    }
+
+    @Override
+    protected View onCreateContentView()
+    {
+        return LayoutInflater.from(context).inflate(
+                context.getResources().getIdentifier(
+                        "dialog_marketing_layout", "layout", context.getPackageName()), null);
+    }
 }
